6b92224a5ce263b49d15af031dd122b386b4b6c5
ChloeM <meinshac@gmail.com>
2022-03-23 00:20:51 -0400

    added comments for the routes im going

--- server/server.js
+++ server/server.js
@@ -6,9 +6,23 @@ app.get('/', (req, res) => {
   res.status(200).send('hello, FEC');
 });
 
+/*must check for page and count params eventually*/
+app.get('qa/questions', (req, res) => {
+  res.status(200).send('hello, I have all your questions!');
+});
 
+//GET /qa/questions/:question_id/answers
+app.get('qa/questions:question_id/answers', (req, res) => {
+  res.status(200).send('hello, answers');
+});
 
-
+// GET qa/answers
+// POST /qa/questions/:question_id/answers route
+// POST /qa/questions
+// PUT /qa/questions/:question_id/helpful
+// PUT /qa/questions/:question_id/report
+// PUT /qa/answers/:answer_id/helpful
+// PUT /qa/answers/:answer_id/report
 // var server = app.listen(port, () => {
 //   console.log(`Now listening on port ${port} at http://localhost:${port}`);
 // });

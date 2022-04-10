37aee3398380fcca6a7abad436cfaa7b548c3b08
ChloeM <meinshac@gmail.com>
2022-04-10 00:39:11 -0400

    fix typos

--- database/queries.js
+++ database/queries.js
@@ -6,3 +6,18 @@ const pool = new Pool({
   password: 'password',
   port: 5432,
 })
+
+
+const getAnswers = (req, res) => {
+  pool.query('SELECT * from answers', (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.row);
+  })
+}
+
+
+module.exports = {
+  getAnswers
+}

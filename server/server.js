38a0ccd31081ae6c2c6e4ff913c50927a7448d09
ChloeM <meinshac@gmail.com>
2022-04-10 00:39:43 -0400

    installed pg

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-
+const db = require('../database/queries.js');
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({

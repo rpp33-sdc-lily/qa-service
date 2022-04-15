e72a8acf7db99d57a5b88262fb57c06fbc0999c9
ChloeM <meinshac@gmail.com>
2022-04-15 17:43:59 -0400

    added env vars for switching dbs

--- database/index.js
+++ database/index.js
@@ -1,14 +1,15 @@
 const Pool = require('pg').Pool
+
 const pool = new Pool({
   user: 'chloe',
   host: 'localhost',
-  database: 'qatestdb',
+  database: process.env.MODE = 'testing' ? 'qatestdb' : 'qa',
   password: 'password',
   port: 5432,
 });
 
 module.exports.query = (text, values) => {
-  console.log('query:', text, Array.isArray(values))
+  // console.log(text, 'values is an array:', Array.isArray(values))
 
   return pool.query(text, values)
 }

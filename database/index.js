833fe80b6cd42317b7c7abb0ef7fa1b8a4e2a129
ChloeM <meinshac@gmail.com>
2022-05-07 18:09:13 -0400

    update server settings

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ var db = process.env.NODE_ENV === 'production' ? 'qa' : 'qatestdb';
 var db_password = require('./db_config.js').DB_PASSWORD;
 console.log('db selected: ', db);
 var pool;
-var db_ip = '184.73.8.60';
+var db_ip = '54.173.55.12';
 if (db === 'qa') {
   pool = new Pool({
     user: 'postgres',
--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(
 app.get('/', (req, res) => {
   res.status(200).json({'info': 'Node.js, Express, and Postgres API'});
 });
-var loaderioKey = 'loaderio-b5d77cca622d6592e978fa05300709c2';
+var loaderioKey = 'loaderio-4c9786b81b83750c9e376f85ba3838d8';
 app.get(`/${loaderioKey}/`, (req, res) => {
   console.log(`sent ${loaderioKey}  back`);
   res.status(200).send(loaderioKey);
